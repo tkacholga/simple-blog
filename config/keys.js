4f6ca2f6bc984ea7d060510beebfec675371514c
Olga Tkach <tkacholya@gmail.com>
2019-01-17 14:36:54 -0500

    changed gatsby.config

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV)
   module.exports = require('./keys_prod')
 } else {
-  console.log(process.env.NODE_ENV)
   module.exports = require('./keys_dev')
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,27 @@
 const keys = require('./config/keys')
 
+let contentfulConfig
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require('./.contentful')
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  )
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -20,11 +42,8 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: keys.spaceId,
-        accessToken: keys.accessToken,
-      },
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
     },
     {
       resolve: `gatsby-transformer-remark`,
