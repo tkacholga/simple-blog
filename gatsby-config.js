801b8d4bdb22429c89e126249e89b69001fef67c
Olga Tkach <tkacholya@gmail.com>
2019-01-17 14:59:47 -0500

    changed gatsby.config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,4 @@
-const keys = require('./config/keys')
-
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
-}
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
@@ -42,8 +20,11 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
